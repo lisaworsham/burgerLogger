e8c397e66df7b9fdd522ee661a8f90de9a2113e5
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-10-05 12:47:36 -0500

    updated controller file and removed devoured in array since it's defaulted in schema

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // import the model (burger.js) to use its database functions.
-var burger = requrie("../models/burger.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
     burger.insertOne([
-        "burger_name", "devoured"
+        "burger_name"
     ], [
-        req.body.burger_name, req.body.devoured
+        req.body.burger_name
     ], function(result) {
         res.json({ id: result.insertId });
     });
