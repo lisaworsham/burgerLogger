eb4af800855cbe434af5be0cb925426e33c72323
Lisa Worsham <lisa.r.hyndman@gmail.com>
2020-10-08 16:20:58 -0500

    updated connection code for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1Yanaeay0922",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "1Yanaeay0922",
+        database: "burgers_db"
+    });
+};
+
 
 // Make connection. "Are we on the Internet?"
 connection.connect(function(err) {
